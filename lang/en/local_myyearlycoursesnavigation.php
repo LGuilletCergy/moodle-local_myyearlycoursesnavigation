54ba670393f093f9698a14485efaff5f16fe44f2
LGuillet <laurent.guillet@u-cergy.fr>
2019-10-10 14:33:47 +0200

    Initial commit

---
+++ lang/en/local_myyearlycoursesnavigation.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Initially developped for :
+ * Université de Cergy-Pontoise
+ * 33, boulevard du Port
+ * 95011 Cergy-Pontoise cedex
+ * FRANCE
+ *
+ * Add a my courses menu to the main navbar
+ *
+ * @package   local_myyearlycoursesnavigation
+ * @copyright 2019 Laurent Guillet <laurent.guillet@u-cergy.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * File : en/myyearlycoursesnavigation.php
+ * English language file
+ */
+
+
+$string['pluginname'] = "My yearly courses navigation";
+$string['privacy:metadata'] = 'The plugin stores no personal data.';
---
+++ lang/fr/local_myyearlycoursesnavigation.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Initially developped for :
+ * Université de Cergy-Pontoise
+ * 33, boulevard du Port
+ * 95011 Cergy-Pontoise cedex
+ * FRANCE
+ *
+ * Add a my courses menu to the main navbar
+ *
+ * @package   local_myyearlycoursesnavigation
+ * @copyright 2019 Laurent Guillet <laurent.guillet@u-cergy.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * File : fr/myyearlycoursesnavigation.php
+ * French language file
+ */
+
+
+$string['pluginname'] = "Navigation Mes cours";
+$string['privacy:metadata'] = 'Le plugin ne stocke aucune donnée personnelle.';
